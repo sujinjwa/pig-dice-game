9ae80e89bb42c623e34b4a7e939eed90c1154f0a
JhyunJ1 <jhyunj1005@gmail.com>
2023-01-13 14:30:00 +0900

    feat: Update username feature on InputView.py
    Add input username feature with Exception

---
+++ view/InputView.py
@@ -0,0 +1,11 @@
+class InputView:
+    def username():
+        while True:
+            username = input("이름을 입력하세요: ")
+            try:
+                username = int(username)
+                print(('이름에 숫자 혹은 문자를 입력하셨습니다.'))
+                continue
+            except ValueError:
+                return username
+
