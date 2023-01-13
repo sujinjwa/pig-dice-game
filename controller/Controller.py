920e1b6ac26ab3b0cc7f68be984c6ab19c750666
sujinjwa <nasujin744@naver.com>
2023-01-13 14:28:53 +0900

    feat: 게임 시작 문장 출력

--- controller/Controller.py
+++ controller/Controller.py
@@ -1,6 +1,9 @@
+from view import OutputView
+
 class Controller:
     def __init__(self):
         pass
 
     def play(self):
-        print('hello')
+        outputView = OutputView.OutputView()
+        outputView.print_start_message()
